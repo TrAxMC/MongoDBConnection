eebf40616ae1619fa1947669b7b9240f5583d214
frame-dev <darrylhuber39@gmail.com>
2021-04-06 19:03:27 +0200

    Update

--- src/main/java/de/framedev/mongodbconnections/main/Main.java
+++ src/main/java/de/framedev/mongodbconnections/main/Main.java
@@ -8,25 +8,17 @@ package de.framedev.mongodbconnections.main;
  * This Class was made at 10.12.2019, 22:51
  */
 
-import com.google.gson.Gson;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+
     private static Main plugin;
-    private MongoManager mongoManager;
+    
     @Override
     public void onEnable() {
         plugin = this;
         this.getConfig().options().copyDefaults(true);
         saveConfig();
-        if (Main.getInstance().getConfig().getBoolean("MongoDB.LocalHost")) {
-            this.mongoManager = new MongoManager();
-            this.mongoManager.connectLocalHost();
-        }
-        if (Main.getInstance().getConfig().getBoolean("MongoDB.Boolean")) {
-            this.mongoManager = new MongoManager();
-            this.mongoManager.connect();
-        }
     }
 
     public static Main getInstance() {
